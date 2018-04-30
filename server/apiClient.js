506fd401d81c2575ed58db8c7a3ebab4f799dc90
Julia <jnmareike@gmail.com>
2018-04-30 23:32:04 +1200

    get ruru st via api

---
+++ server/apiClient.js
@@ -0,0 +1,18 @@
+const request = require('superagent')
+
+const key = 'ecddf4ae567846ed8236d226dd412cea'
+const ruruUrl = `https://data.linz.govt.nz/services/query/v1/feature.dojodata/53382/?v=1.3&key=${key}&query={"road_id":1831584}`
+// const linzUrl = 'https://data.linz.govt.nz/services/query/v1/feature.dojodata/53382/?v=1.3'
+
+module.exports = {
+  getRuru
+}
+
+function getRuru () {
+  return request.get(ruruUrl)
+    // .send(`key=${linzKey}`, 'query={"road_id":1831547}', 'include_geom=true')
+    .send({include_geom: 'true'})
+    // .then(res => {
+    //   console.log(res.body)
+    // })
+}
--- server/routes/linz.js
+++ server/routes/linz.js
@@ -1,9 +1,17 @@
 const express = require('express')
 
+const getRuru = require('../apiClient').getRuru
+
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.send('hello')
+  getRuru()
+    .then(response => {
+      res.send(response.body.items)
+    })
+    .catch((err) => {
+      res.status(500).send(err.message)
+    })
 })
 
 module.exports = router
