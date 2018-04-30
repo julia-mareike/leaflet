d967108ff227e85048f62e7118ba08c182cca32e
Julia <jnmareike@gmail.com>
2018-05-01 00:53:26 +1200

    hard-code api url for now

--- server/apiClient.js
+++ server/apiClient.js
@@ -1,7 +1,7 @@
 const request = require('superagent')
 
 const key = 'ecddf4ae567846ed8236d226dd412cea'
-const ruruUrl = `https://data.linz.govt.nz/services/query/v1/feature.dojodata/53382/?v=1.3&key=${key}&query={"road_id":1831584}`
+const ruruUrl = `https://data.linz.govt.nz/services/query/v1/feature.dojodata/53382/?v=1.3&key=${key}&query={"road_id":1831584}&include_geom="true"`
 // const linzUrl = 'https://data.linz.govt.nz/services/query/v1/feature.dojodata/53382/?v=1.3'
 
 module.exports = {
@@ -11,8 +11,8 @@ module.exports = {
 function getRuru () {
   return request.get(ruruUrl)
     // .send(`key=${linzKey}`, 'query={"road_id":1831547}', 'include_geom=true')
-    .send({include_geom: 'true'})
+    // .send({include_geom: 'true'})
     // .then(res => {
-    //   console.log(res.body)
+    //   console.log('done')
     // })
 }
