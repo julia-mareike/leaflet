ca544fa4c37413c1bf72174b6cda9338f97551e5
Julia <jnmareike@gmail.com>
2018-04-30 22:27:45 +1200

    setting up external api

--- client/components/Auckland.jsx
+++ client/components/Auckland.jsx
@@ -1,20 +1,17 @@
 import React from 'react'
-import { FeatureGroup, LayerGroup, LayersControl, Map, Polyline, TileLayer } from 'react-leaflet'
+import {LayersControl, Map, Polyline, TileLayer} from 'react-leaflet'
 
-const { BaseLayer, Overlay } = LayersControl
+import {ruru, rākau} from '../streets'
 
-import { ruru, rākau } from '../streets'
+const {BaseLayer, Overlay} = LayersControl
 
 class Auckland extends React.Component {
-
   render () {
     const position = [-36.8670921833, 174.7585096333]
     const zoom = 15
     const url = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}'
     const attribution = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>'
     const accessToken = 'pk.eyJ1IjoianVsaWEtbWFyZWlrZSIsImEiOiJjamdhY2VyNHoxMXFyMnlwam05dWphd3Y4In0.yAxAhojYaX8Q4Xcf-Qh2JQ'
-    
-    
 
     return (
       <Map center={position} zoom={zoom}>
@@ -28,10 +25,10 @@ class Auckland extends React.Component {
             />
           </BaseLayer>
           <Overlay name='ruru'>
-          <Polyline color='blue' positions={ruru} />
+            <Polyline color='blue' positions={ruru} />
           </Overlay>
           <Overlay name='rākau'>
-          <Polyline color='lime' positions={rākau} />
+            <Polyline color='lime' positions={rākau} />
           </Overlay>
         </LayersControl>
       </Map>
--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 const path = require('path')
 const express = require('express')
 
+const linzRoutes = require('./routes/linz')
+
 const server = express()
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, '../public')))
+
+server.use('/api/v1/linz', linzRoutes)
 
 module.exports = server
