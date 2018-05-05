ca506334828b68604a63d6caf1c2e9a3bd6f7a3a
Julia <jnmareike@gmail.com>
2018-05-05 23:47:31 +1200

    set up test to reverse coordinates

--- tests/apiClient.test.js
+++ tests/apiClient.test.js
@@ -1,5 +1,14 @@
-// const apiClient = require('../client/apiClient')
+import {reverseCoords} from '../client/apiClient'
+import {nikau} from '../client/streets'
 
 test('test is testy', () => {
   expect(true).toBeTruthy()
+})
+
+test('nested arrays can be reversed', () => {
+  const actual = reverseCoords(nikau)
+
+  const expected = []
+
+  expect(actual).toBe(expected)
 })
