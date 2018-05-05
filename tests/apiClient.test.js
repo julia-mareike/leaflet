f40ac9dfe1b94b2003d246401c86ac2822618030
Julia <jnmareike@gmail.com>
2018-05-06 01:24:16 +1200

    set up test data, reverse tests passing

--- tests/apiClient.test.js
+++ tests/apiClient.test.js
@@ -1,14 +1,39 @@
 import {reverseCoords} from '../client/apiClient'
-import {nikau} from '../client/streets'
+import {ruru, horopito} from './streets'
 
 test('test is testy', () => {
   expect(true).toBeTruthy()
 })
 
-test('nested arrays can be reversed', () => {
-  const actual = reverseCoords(nikau)
+test('nested arrays of length 1 can be reversed', () => {
+  const actual = reverseCoords(horopito)
 
-  const expected = []
+  const expected = [
+    [
+      [-36.8705444333, 174.7528078667],
+      [-36.8706045167, 174.75306285],
+      [-36.8708986167, 174.7544375833]
+    ]
+  ]
 
-  expect(actual).toBe(expected)
+  expect(actual).toEqual(expected)
+})
+
+test('nested arrays of length 2 can be reversed', () => {
+  const actual = reverseCoords(ruru)
+
+  const expected = [
+    [
+      [-36.8670921833, 174.7585096333],
+      [-36.8674314167, 174.75874785],
+      [-36.8674576167, 174.7590423167]
+    ],
+    [
+      [-36.866171316700004, 174.75781668330004],
+      [-36.866263, 174.7579059333],
+      [-36.8670921833, 174.7585096333]
+    ]
+  ]
+
+  expect(actual).toEqual(expected)
 })
