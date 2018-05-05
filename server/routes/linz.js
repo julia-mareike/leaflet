f71133375e35a638f9b624f5c507dd66f0683905
Julia <jnmareike@gmail.com>
2018-05-05 23:58:01 +1200

    can display individual polylines for almost all roads in vector query

--- server/routes/linz.js
+++ server/routes/linz.js
@@ -8,7 +8,7 @@ router.get('/', (req, res) => {
   getCoords()
     .then(response => {
       // res.send(response.body.items[0].__geometry__.coordinates) // ruru
-      res.send(response.body.vectorQuery.layers[53382].features[0].geometry.coordinates) // vector
+      res.send(response.body.vectorQuery.layers[53382].features[1].geometry.coordinates) // vector
     })
     .catch(() => {
       res.sendStatus(500)
