58659bff9d631130cb1a5b80d056c5c7903a0777
Julia <jnmareike@gmail.com>
2018-05-05 23:59:42 +1200

    add note about which roads arent displaying

--- server/routes/linz.js
+++ server/routes/linz.js
@@ -9,6 +9,7 @@ router.get('/', (req, res) => {
     .then(response => {
       // res.send(response.body.items[0].__geometry__.coordinates) // ruru
       res.send(response.body.vectorQuery.layers[53382].features[1].geometry.coordinates) // vector
+      // can't display [3, 5, 6, 13, 14, 23, 25, 29]
     })
     .catch(() => {
       res.sendStatus(500)
