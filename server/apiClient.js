0bcffcaef4b8684ba5c7ba079c650edff0061522
Julia <jnmareike@gmail.com>
2018-05-06 02:15:01 +1200

    remove comments and road id queries

--- server/apiClient.js
+++ server/apiClient.js
@@ -1,22 +1,17 @@
 const request = require('superagent')
 
 const key = 'ecddf4ae567846ed8236d226dd412cea'
-// const ruru = '{"road_id":1831464}'
-// const ruruUrl = 'https://data.linz.govt.nz/services/query/v1/feature.dojodata/53382/'
 const radiusUrl = 'https://data.linz.govt.nz/services/query/v1/vector.json'
 
 function getCoords () {
   return request.get(radiusUrl)
     .query({key: key})
-    .query({layer: 53382}) // vector
-    .query({x: 174.7585488}) // vector
-    .query({y: -36.8673986}) // vector
-    .query({max_results: 30}) // vector
-    .query({radius: 1000}) // vector
-    .query({geometry: true}) // vector
-    // .query({v: 1.3}) // ruru
-    // .query({query: ruru}) // ruru
-    // .query({include_geom: true}) // ruru
+    .query({layer: 53382})
+    .query({x: 174.7585488})
+    .query({y: -36.8673986})
+    .query({max_results: 100})
+    .query({radius: 1000})
+    .query({geometry: true})
 }
 
 module.exports = {
--- server/routes/linz.js
+++ server/routes/linz.js
@@ -7,9 +7,7 @@ const router = express.Router()
 router.get('/', (req, res) => {
   getCoords()
     .then(response => {
-      // res.send(response.body.items[0].__geometry__.coordinates) // ruru
-      res.send(response.body.vectorQuery.layers[53382].features[1].geometry.coordinates) // vector
-      // can't display [3, 5, 6, 13, 14, 23, 25, 29]
+      res.send(response.body.vectorQuery.layers[53382].features)
     })
     .catch(() => {
       res.sendStatus(500)
