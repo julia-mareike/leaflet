2d00234bb1d94fbf59f35a272689fd1cb7f244b6
Julia <jnmareike@gmail.com>
2018-05-07 16:25:19 +1200

    send centre coords via api query

--- server/apiClient.js
+++ server/apiClient.js
@@ -3,12 +3,13 @@ const request = require('superagent')
 const key = 'ecddf4ae567846ed8236d226dd412cea'
 const radiusUrl = 'https://data.linz.govt.nz/services/query/v1/vector.json'
 
-function getCoords () {
+function getCoords (coords) {
   return request.get(radiusUrl)
     .query({key: key})
     .query({layer: 53382})
-    .query({x: 174.7585488})
-    .query({y: -36.8673986})
+    // may need to reverse xy/latlongs later
+    .query({x: coords[1]})
+    .query({y: coords[0]})
     .query({max_results: 100})
     .query({radius: 1000})
     .query({geometry: true})
